c2674e088a5d6b0e8f05330483fcf96bf4949d10
Marina <gaspertrishka@mail.ru>
2022-10-23 15:20:29 +0700

    задание 1

---
+++ Home work01/Program.cs
@@ -0,0 +1,25 @@
+﻿ /*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+
+
+bool stop = false;
+int number = 0;
+int summ = 0;
+
+while(!stop)// !false = true; !true = false
+{
+Console.WriteLine("введите число");
+number = int.Parse(Console.ReadLine());
+
+if(number > 0)
+summ++;
+
+Console.WriteLine("Продолжить вводить числа? y/n");
+string result = Console.ReadLine();
+if(result == "y")
+stop = false;
+else
+stop = true;
+}
+Console.WriteLine($"Кол-во чисел, введеных пользователем, которые больше 0 - {summ}");
